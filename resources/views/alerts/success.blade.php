ebe949de64fd2b65ac14d5df5dcf0adf6e863456
BryanSuarez <bryanbolivar.1992@gmail.com>
2016-05-01 18:41:43 -0500

    Actualización
    agregue signos de exclamación en la alerta de éxito para probar el pull del versionado

--- resources/views/alerts/success.blade.php
+++ resources/views/alerts/success.blade.php
@@ -2,6 +2,6 @@
     <div class="alert alert-success alert-dismissable">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
         <h4><i class="icon fa fa-check"></i> Éxito!</h4>
-        Tarea agregada correctamente.
+        Tarea agregada correctamente!!!.
     </div>
 @endif
