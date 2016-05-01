f5e360b6fd3d90bed4513bab78b2add45d312052
email <bryanbolivar.1992@gmail.com>
2016-05-01 18:39:41 -0500

    Cambios en parcial de alertas para  probar el versionado

--- resources/views/alerts/errors.blade.php
+++ resources/views/alerts/errors.blade.php
@@ -1,7 +1,7 @@
 @if(count($errors) > 0)
     <div class="alert alert-danger alert-dismissable">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        <h4><i class="icon fa fa-ban"></i> Alerta!</h4>
+        <h4><i class="icon fa fa-ban"></i> Alerta! de error</h4>
         <ul>
             @foreach($errors->all() as $error)
                 <li>{!! $error !!}</li>
