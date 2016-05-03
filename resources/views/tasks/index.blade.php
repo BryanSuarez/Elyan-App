a920bacee6af0db76bd2291d2d1d97e875d30232
email <bryanbolivar.1992@gmail.com>
2016-05-03 17:49:02 -0500

    Implementé mis datos en el footer de autoría de la vista tasks.index

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -352,7 +352,7 @@
         <div class="pull-right hidden-xs">
             <b>Version</b> 2.3.0
         </div>
-        <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
+        <strong>Copyright &copy; 2016 <a href="#">Bryan Suárez</a>.</strong> Quito-Ecuador
         reserved.
     </footer>
 
